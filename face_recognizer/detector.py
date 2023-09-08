0d68324149e7e68e505980fe03e970aee72cb513
Jacob Munjanattu <115675011+jacob9877@users.noreply.github.com>
2023-09-07 22:00:09 -0400

    Update detector.py
    added user registration functions

--- face_recognizer/detector.py
+++ face_recognizer/detector.py
@@ -1,10 +1,13 @@
 from pathlib import Path
+import sqlite3
 from collections import Counter
 from PIL import Image, ImageDraw
 import face_recognition
 import pickle
 import argparse
 
+DATABASE_FILE = "user_database.db"
+IMAGE_FOLDER = "user_images"
 DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
 BOUNDING_BOX_COLOR = "blue"
 TEXT_COLOR = "white"
@@ -15,6 +18,8 @@ parser.add_argument("--validate", action="store_true", help="Validate trained mo
 parser.add_argument("--test", action="store_true", help="Test the model with an unknown image")
 parser.add_argument("m", action="store", default="hog", choices=["hog", "cnn"], help="which model to use for training: hog (CPU), cnn (GPU)")
 parser.add_argument("-f", action="store", help="Path to an image with an unknown face")
+parser.add_argument("--register", action="store_true", help="Register a new user")
+parser.add_argument("--username", action="store", help="Username for user registration")
 args = parser.parse_args()
 Path("training").mkdir(parents=True, exist_ok=True)
 Path("output").mkdir(parents=True, exist_ok=True)
@@ -96,3 +101,49 @@ if __name__ == "__main__":
         validate(model=args.m)
     if args.test:
         recognize_faces(image_location=args.f, model=args.m)
+############################################################################################################################################
+#User Registration: Implement a mechanism for users to register their faces in the system. This could involve capturing and storing their facial images 
+#along with user profiles.
+
+# User Registration Functions
+def create_user_table():
+    conn = sqlite3.connect(DATABASE_FILE)
+    cursor = conn.cursor()
+
+    # Create the user table if it doesn't exist
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users
+                      (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       username TEXT NOT NULL UNIQUE,
+                       encoding TEXT NOT NULL)''')
+
+    conn.commit()
+    conn.close()
+
+def register_user(username, image_path):
+    # Load the user image and generate a face encoding
+    user_image = face_recognition.load_image_file(image_path)
+    face_encoding = face_recognition.face_encodings(user_image)[0]
+
+    # Store the face encoding in the database
+    conn = sqlite3.connect(DATABASE_FILE)
+    cursor = conn.cursor()
+
+    # Insert the user's data into the database
+    cursor.execute("INSERT INTO users (username, encoding) VALUES (?, ?)", (username, str(face_encoding)))
+
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    if args.train:
+        encode_known_faces(model=args.m)
+    if args.validate:
+        validate(model=args.m)
+    if args.test:
+        recognize_faces(image_location=args.f, model=args.m)
+    if args.register and args.username:
+        create_user_table()
+        register_user(args.username, args.f)
+
+
+        
