7a930c23a0529b1d07f2a9b561965248cf9062b4
AlbertJojo2313 <46793650+AlbertJojo2313@users.noreply.github.com>
2023-09-13 21:47:46 -0400

    Update look_Up.py

--- face_recognizer/look_Up.py
+++ face_recognizer/look_Up.py
@@ -1,1 +1,40 @@
+from pathlib import Path
+
+def main():
+    print("Looks up picture")
+    print("1. Puts images for training")
+    print("2. Copies the trained images to validate")
+    print("3. Test the model")
+
+    choice = int(input("Enter choice based on options: "))
+
+    while choice <= 3:
+        if choice == 1:
+            # Call a function to look up pictures and download them, then place them in the training folder
+            download_and_place_in_training()
+        elif choice == 2:
+            # Call a function to copy the training images to the validation folder
+            copy_training_to_validation()
+        elif choice == 3:
+            # Call a function to copy the training images to the test folder
+            copy_training_to_test()
+        else:
+            print("Invalid choice. Please choose a valid option.")
+        
+        choice = int(input("Enter choice based on options: "))
+
+def download_and_place_in_training():
+    # Implement the logic to download and place pictures in the training folder
+    pass
+
+def copy_training_to_validation():
+    # Implement the logic to copy the training images to the validation folder
+    pass
+
+def copy_training_to_test():
+    # Implement the logic to copy the training images to the test folder
+    pass
+
+if __name__ == "__main__":
+    main()
 
