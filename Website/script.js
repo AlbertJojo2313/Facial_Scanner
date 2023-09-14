739f23338255097bab2f3055e6bc50e040ba8b57
Jacob Munjanattu <115675011+jacob9877@users.noreply.github.com>
2023-09-13 23:38:33 -0400

    Update script.js

--- Website/script.js
+++ Website/script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("myButton");
+    button.addEventListener("click", function () {
+        alert("Hello, friends! This is my website.");
+    });
+});
 
