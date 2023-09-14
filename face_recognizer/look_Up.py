6f6c7493c9b91b94382bbb7cadecb4812dba2b7f
AlbertJojo2313 <46793650+AlbertJojo2313@users.noreply.github.com>
2023-09-13 22:07:26 -0400

    Update look_Up.py

--- face_recognizer/look_Up.py
+++ face_recognizer/look_Up.py
@@ -25,14 +25,22 @@ def option():
 
 def download_and_place_in_training():
     # Implement the logic to download and place pictures in the training folder
+    # Also asks on how many pictures to download.
+    # Should be linked to google images.
+    # Check if folder with that name exists in the directory.
+    # -- If yes then just place the pictures in the subdirectory.
+    #  --- Else create a folder with the name of the subject then places the pictures in the directory.
+    # Saves all downloaded pictures as jpeg.
     pass
 
 def copy_training_to_validation():
     # Implement the logic to copy the training images to the validation folder
+    # Copies all sub-directories that are present in training folder to the validation folder.
     pass
 
 def copy_training_to_test():
     # Implement the logic to copy the training images to the test folder
+    # Up to consideration.
     pass
 
 if __name__ == "__main__":
