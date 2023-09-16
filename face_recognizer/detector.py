8019dd131ce29f066cd249190eff7d3a3928e248
AlbertJojo2313 <46793650+AlbertJojo2313@users.noreply.github.com>
2023-09-15 22:37:41 -0400

    Update detector.py
    Added helpful comments to this code.

--- face_recognizer/detector.py
+++ face_recognizer/detector.py
@@ -12,6 +12,7 @@ DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
 BOUNDING_BOX_COLOR = "blue"
 TEXT_COLOR = "white"
 
+# Create an argument parser to handle command line arguments
 parser = argparse.ArgumentParser(description="Recognize faces in an image")
 parser.add_argument("--train", action="store_true", help="Train on input data")
 parser.add_argument("--validate", action="store_true", help="Validate trained model")
@@ -21,10 +22,13 @@ parser.add_argument("-f", action="store", help="Path to an image with an unknown
 parser.add_argument("--register", action="store_true", help="Register a new user")
 parser.add_argument("--username", action="store", help="Username for user registration")
 args = parser.parse_args()
+
+# Create necessary directories if they don't exist
 Path("training").mkdir(parents=True, exist_ok=True)
 Path("output").mkdir(parents=True, exist_ok=True)
 Path("validation").mkdir(parents=True, exist_ok=True)
 
+# Function to encode known faces and store them in a pickle file
 def encode_known_faces(model="hog", encodings_location=DEFAULT_ENCODINGS_PATH):
     names = []
     encodings = []
@@ -32,10 +36,9 @@ def encode_known_faces(model="hog", encodings_location=DEFAULT_ENCODINGS_PATH):
         name = filepath.parent.name
         image = face_recognition.load_image_file(filepath)
 
-        # Uses face_recognition.face_locations to detect the locations of faces in each image.
-        # Returns a list of four-element tuples, one tuple for each detected face
+        # Detect the locations of faces in each image
         face_locations = face_recognition.face_locations(image, model=model)
-        # Uses face_recognition.face_encodings to generate encodings for the detected faces in an image
+        # Generate encodings for the detected faces in an image
         face_encodings = face_recognition.face_encodings(image, face_locations)
 
         for encoding in face_encodings:
@@ -46,6 +49,7 @@ def encode_known_faces(model="hog", encodings_location=DEFAULT_ENCODINGS_PATH):
     with encodings_location.open(mode="wb") as f:
         pickle.dump(name_encodings, f)
 
+# Function to recognize faces in an image and display bounding boxes and names
 def recognize_faces(image_location, model="hog", encodings_location=DEFAULT_ENCODINGS_PATH):
     with encodings_location.open(mode="rb") as f:
         loaded_encodings = pickle.load(f)
@@ -53,10 +57,9 @@ def recognize_faces(image_location, model="hog", encodings_location=DEFAULT_ENCO
     input_face_locations = face_recognition.face_locations(input_image, model=model)
     input_face_encodings = face_recognition.face_encodings(input_image, input_face_locations)
 
-    pillow_image = Image.fromarray(input_image)  # Creates a pillow image object from your loaded input image
-    draw = ImageDraw.Draw(pillow_image)  # creates an ImageDraw object, which draws a bounding box around detected faces
+    pillow_image = Image.fromarray(input_image)
+    draw = ImageDraw.Draw(pillow_image)
 
-    # Iterates through input_face_locations and input_face_encodings in parallel using zip().
     for bounding_box, unknown_encoding in zip(input_face_locations, input_face_encodings):
         name = _recognize_face(unknown_encoding, loaded_encodings)
         if not name:
@@ -65,6 +68,7 @@ def recognize_faces(image_location, model="hog", encodings_location=DEFAULT_ENCO
     del draw
     pillow_image.show()
 
+# Function to recognize a single face
 def _recognize_face(unknown_encoding, loaded_encodings):
     boolean_matches = face_recognition.compare_faces(loaded_encodings["encodings"], unknown_encoding)
     votes = Counter(
@@ -75,6 +79,7 @@ def _recognize_face(unknown_encoding, loaded_encodings):
     if votes:
         return votes.most_common(1)[0][0]
 
+# Function to display the face with a bounding box and name
 def _display_face(draw, bounding_box, name):
     top, right, bottom, left = bounding_box
     draw.rectangle(((left, top), (right, bottom)), outline=BOUNDING_BOX_COLOR)
@@ -89,6 +94,7 @@ def _display_face(draw, bounding_box, name):
         fill="white",
     )
 
+# Function to validate the model
 def validate(model="hog"):
     for filepath in Path("validation").rglob("*"):
         if filepath.is_file():
@@ -101,9 +107,6 @@ if __name__ == "__main__":
         validate(model=args.m)
     if args.test:
         recognize_faces(image_location=args.f, model=args.m)
-############################################################################################################################################
-#User Registration: Implement a mechanism for users to register their faces in the system. This could involve capturing and storing their facial images 
-#along with user profiles.
 
 # User Registration Functions
 def create_user_table():
@@ -144,6 +147,3 @@ if __name__ == "__main__":
     if args.register and args.username:
         create_user_table()
         register_user(args.username, args.f)
-
-
-        
